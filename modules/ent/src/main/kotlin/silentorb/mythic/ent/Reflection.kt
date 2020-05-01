d98c05e4e147c7ca957ba5d9a24ac415d04fd1a4
cj <christopherwjohnson@gmail.com>
2020-05-01 17:40:44 -0600

    Added a property reflection function that returns a Map instead of a List.

--- modules/ent/src/main/kotlin/silentorb/mythic/ent/Reflection.kt
+++ modules/ent/src/main/kotlin/silentorb/mythic/ent/Reflection.kt
@@ -2,5 +2,12 @@ package silentorb.mythic.ent
 
 inline fun <reified T : Any> reflectProperties(source: Any): List<T> = source::class.java.kotlin.members
     .filter {
-      it.returnType.classifier == T::class }
+      it.returnType.classifier == T::class
+    }
     .map { it.call(source) as T }
+
+inline fun <reified T : Any> reflectPropertiesMap(source: Any): Map<String, T> = source::class.java.kotlin.members
+    .filter {
+      it.returnType.classifier == T::class
+    }
+    .associate { Pair(it.name, it.call(source) as T) }
