d84b6056d279beab2b31a38c714fba8e3728ae22
cj <christopherwjohnson@gmail.com>
2020-05-25 21:26:42 -0600

    Added code to sample fathom by cells.

--- modules/imaging/src/main/kotlin/silentorb/mythic/imaging/fathoming/sampling/Sampling.kt
+++ modules/imaging/src/main/kotlin/silentorb/mythic/imaging/fathoming/sampling/Sampling.kt
@@ -3,9 +3,40 @@ package silentorb.mythic.imaging.fathoming.sampling
 import silentorb.mythic.imaging.fathoming.surfacing.GridBounds
 import silentorb.mythic.imaging.fathoming.surfacing.snapToSurfaceIncludingNormal
 import silentorb.mythic.spatial.Vector3
+import silentorb.mythic.spatial.Vector3i
 import silentorb.mythic.spatial.Vector4
 import silentorb.mythic.spatial.toVector3
 import kotlin.math.abs
+
+fun samplePoint(config: SamplingConfig, start: Vector3): SamplePoint? {
+  val resolution = config.resolution
+  val sampleRange = 1.5f / resolution
+  val getDistance = config.getDistance
+  val getColor = config.getColor
+  val pointSize = config.pointSize / resolution
+
+  val startingDistance = getDistance(start)
+  return if (abs(startingDistance) > sampleRange)
+    null
+  else {
+    val (location, normal) = snapToSurfaceIncludingNormal(getDistance, start)
+    val rgb = getColor(location)
+    SamplePoint(
+        location = location,
+        normal = normal,
+        color = Vector4(rgb.x, rgb.y, rgb.z, 1f),
+        size = pointSize
+    )
+  }
+}
+
+fun indexToVector3i(sliceSize: Int, xSize: Int, index: Int): Vector3i {
+  val z = index / sliceSize
+  val zRemainder = index - sliceSize * z
+  val y = zRemainder / xSize
+  val x = zRemainder - y * xSize
+  return Vector3i(x, y, z)
+}
 
 fun sampleFunction(config: SamplingConfig, bounds: GridBounds): List<SamplePoint> {
   val boundsDimensions = bounds.end - bounds.start
@@ -15,35 +46,41 @@ fun sampleFunction(config: SamplingConfig, bounds: GridBounds): List<SamplePoint
     return listOf()
 
   val resolution = config.resolution
-  val sampleRange = 1.5f / resolution
-  val getDistance = config.getDistance
-  val getColor = config.getColor
   val stepDimensions = boundsDimensions * config.resolution
   val sampleCount = stepDimensions.x * stepDimensions.y * stepDimensions.z
   val sliceSize = stepDimensions.x * stepDimensions.y
   val start = bounds.start.toVector3()
-  val pointSize = config.pointSize / resolution
 
-  return (0 until sampleCount).mapNotNull { i ->
-    val z = i / sliceSize
-    val zRemainder = i - sliceSize * z
-    val y = zRemainder / stepDimensions.x
+  return (0 until sampleCount).mapNotNull { index ->
+    val offset = indexToVector3i(sliceSize, stepDimensions.x, index).toVector3()
+    val startingLocation = start + offset / resolution.toFloat()
+    samplePoint(config, startingLocation)
+  }
+}
 
-    val x = zRemainder - y * stepDimensions.x
+data class CellSampler(
+    val cellCount: Int,
+    val sampler: (Int) -> List<SamplePoint>
+)
 
-    val startingLocation = start + Vector3(x.toFloat(), y.toFloat(), z.toFloat()) / resolution.toFloat()
-    val startingDistance = getDistance(startingLocation)
-    if (abs(startingDistance) > sampleRange)
-      null
-    else {
-      val (location, normal) = snapToSurfaceIncludingNormal(getDistance, startingLocation)
-      val rgb = getColor(location)
-      SamplePoint(
-          location = location,
-          normal = normal,
-          color = Vector4(rgb.x, rgb.y, rgb.z, 1f),
-          size = pointSize
-      )
+fun sampleCells(config: SamplingConfig, bounds: GridBounds, cellDivisions: Int = 1): CellSampler {
+  val unitDimensions = bounds.end - bounds.start
+  val subDimensions = unitDimensions * cellDivisions
+  val cellCount = subDimensions.x * subDimensions.y * subDimensions.z
+  val cellsSliceSize = subDimensions.x * subDimensions.y
+  val cellsSizeX = subDimensions.x
+
+  val cellLength = config.resolution
+  val cellSliceSize = cellLength * cellLength
+  val cellSampleCount = cellLength * cellLength * cellLength
+  val resolution = config.resolution.toFloat()
+  val start = bounds.start.toVector3()
+
+  return CellSampler(cellCount) { step ->
+    val cellStart = start + indexToVector3i(cellsSliceSize, cellsSizeX, step).toVector3()
+    (0 until cellSampleCount).mapNotNull { index ->
+      val offset = indexToVector3i(cellSliceSize, cellLength, index).toVector3()
+      samplePoint(config, cellStart + offset / resolution)
     }
   }
 }
