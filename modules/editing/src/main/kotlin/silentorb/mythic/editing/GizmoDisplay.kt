979765a5a88b785465f1bde833282a1155572429
cj <christopherwjohnson@gmail.com>
2020-11-01 13:53:40 -0700

    Refined the 3d compass.

--- modules/editing/src/main/kotlin/silentorb/mythic/editing/GizmoDisplay.kt
+++ modules/editing/src/main/kotlin/silentorb/mythic/editing/GizmoDisplay.kt
@@ -8,7 +8,7 @@ import silentorb.mythic.spatial.*
 typealias ScreenTransform = (Vector3) -> Vector2
 
 fun transformPoint(transform: Matrix, dimensions: Vector2, offset: Vector2): ScreenTransform = { point ->
-  transformToScreen(transform, point)!! * Vector2(1f, -1f) * dimensions + offset
+  transformToScreen(transform, point)!! * Vector2(1f, -2f) * dimensions + offset
 }
 
 fun drawCompass(transform: ScreenTransform, drawList: ImDrawList) {
@@ -23,12 +23,18 @@ fun drawCompass(transform: ScreenTransform, drawList: ImDrawList) {
       Vector3(0f, 0f, 1f),
   )
   val start = transform(Vector3.zero)
+  val black = ImColor.intToColor(0, 0, 0, 255)
 
   for (i in 0 until 3) {
-    val end = transform(vectors[i] * 1f)
-    drawList.addLine(start.x, start.y, end.x, end.y, colors[i])
-    val textPoint = transform(vectors[i] * 1.1f)
-    drawList.addText(textPoint.x - 5f, textPoint.y - 5f, colors[i], ('X' + i).toString())
+    val end = transform(vectors[i] * 2f)
+    drawList.addLine(start.x, start.y, end.x, end.y, colors[i], 3f)
+  }
+  drawList.addCircleFilled(start.x, start.y, 3f, ImColor.intToColor(128, 128, 128, 255))
+
+  for (i in 0 until 3) {
+    val end = transform(vectors[i] * 2f)
+    drawList.addCircleFilled(end.x, end.y, 9f, colors[i])
+    drawList.addText(end.x - 4f, end.y - 6f, black, ('X' + i).toString())
   }
 }
 
@@ -39,9 +45,9 @@ fun drawEditor3dElements(editor: Editor, viewport: Vector4i, camera: CameraRig) 
       100f, ImColor.intToColor(64, 64, 64, 255), 32, 2f)
 
   val dimensions = viewport.zw()
-  val viewTransform = createViewMatrix(camera.location, camera.orientation)
-  val orthoTransform = createPerspectiveMatrix(dimensions, 45f, 0.01f, 1000f) * viewTransform
-  val compassPadding = 40
+  val viewTransform = createViewMatrix(Vector3.zero, camera.orientation)
+  val orthoTransform = createOrthographicMatrix(dimensions, 45f, 0.01f, 1000f) * viewTransform
+  val compassPadding = 50
   val compassOffset = Vector2i(viewport.x + compassPadding, viewport.y + viewport.w - compassPadding)
   val compassTransform = transformPoint(orthoTransform, dimensions.toVector2(), compassOffset.toVector2())
   drawCompass(compassTransform, drawList)
