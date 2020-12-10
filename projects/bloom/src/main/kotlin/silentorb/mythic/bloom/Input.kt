7694742dd3f671a626e9580f81086583e54452aa
cj <christopherwjohnson@gmail.com>
2020-12-09 21:26:26 -0700

    Fixed a bug with the new onClick code where I had forgotten to add back the isInBounds check.

--- projects/bloom/src/main/kotlin/silentorb/mythic/bloom/Input.kt
+++ projects/bloom/src/main/kotlin/silentorb/mythic/bloom/Input.kt
@@ -1,10 +1,11 @@
 package silentorb.mythic.bloom
 
+import silentorb.mythic.bloom.old.isInBounds
 import silentorb.mythic.haft.HaftCommand
 import silentorb.mythic.happenings.Commands
 
 fun onClick(handler: () -> Commands): InputHandler = { input, bounds ->
-  if (input.commands.any { it.type == HaftCommand.leftMouseClick })
+  if (input.commands.any { it.type == HaftCommand.leftMouseClick } && isInBounds(input.mousePosition, bounds))
     handler()
   else
     listOf()
