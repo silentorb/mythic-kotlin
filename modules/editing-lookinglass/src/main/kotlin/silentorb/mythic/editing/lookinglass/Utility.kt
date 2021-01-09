b0d89629083de02901a41b7b8bd22c4f6ad1a4d1
cj <christopherwjohnson@gmail.com>
2021-01-08 21:50:46 -0700

    Editor: Turns out the node mesh gathering code for selection hightlight was convoluted.  Now that code is simplified.

--- modules/editing-lookinglass/src/main/kotlin/silentorb/mythic/editing/lookinglass/Utility.kt
+++ modules/editing-lookinglass/src/main/kotlin/silentorb/mythic/editing/lookinglass/Utility.kt
@@ -18,37 +18,13 @@ import silentorb.mythic.spatial.Matrix
 typealias MeshNodes = List<Pair<Key, Matrix>>
 
 fun getSelectionMeshes(editor: Editor, graph: Graph, node: Key): List<Pair<Key, Matrix>> {
-//  val type = getGraphValue<Key>(graph, node, SceneProperties.type)
-//  val subGraph2 = if (type != null && editor.graphLibrary.containsKey(type))
-//    expandInstances(getExpansionLibrary(editor), editor.graphLibrary[type]!!)
-//  else
-//    null
   val nodes = gatherChildren(graph, node) + node
   val subGraph = graph.filter { nodes.contains(it.source) }
-  val localMesh = getGraphValue<Key>(graph, node, SceneProperties.mesh)
 
-  val subGraphMeshNodes = if (subGraph != null)
-    mapByProperty<Key>(subGraph, SceneProperties.mesh)
-  else
-    mapOf()
-
-  val selfMeshNodes = if (localMesh != null)
-    mapOf(node to localMesh)
-  else
-    mapOf()
-
-  val localTransform = Matrix.identity // getNodeTransform(graph, node)
-  return selfMeshNodes
-      .map { (_, mesh) ->
-        mesh to localTransform
+  return mapByProperty<Key>(subGraph, SceneProperties.mesh)
+      .map { (key, mesh) ->
+        mesh to getNodeTransform(graph, key)
       }
-      .plus(
-          subGraphMeshNodes
-              .map { (key, mesh) ->
-                // TODO: For some reason the matrix integration is needing to be backwards from the main rendering pass
-                mesh to getNodeTransform(graph, key)
-              }
-      )
 }
 
 fun renderMeshNodes(sceneRenderer: SceneRenderer, material: Material, meshNodes: MeshNodes) {
