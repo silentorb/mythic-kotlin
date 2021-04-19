58e6493288146ee634719458060cdec83b94c339
cj <christopherwjohnson@gmail.com>
2021-04-18 23:21:39 -0600

    Moved UseAction to marloth.

--- modules/happenings/src/main/kotlin/silentorb.mythic/happenings/UseAction.kt
+++
@@ -1,9 +0,0 @@
-package silentorb.mythic.happenings
-
-import silentorb.mythic.ent.Id
-
-data class UseAction(
-    val actor: Id,
-    val action: Id,
-    val deferredEvents: Map<String, GameEvent> = mapOf()
-)
